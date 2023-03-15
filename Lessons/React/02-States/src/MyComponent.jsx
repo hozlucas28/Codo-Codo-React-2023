6b4af582e560ed095a983a87232dea8049aac352
hozlucas28 <hozlucas28@gmail.com>
2023-03-15 19:29:47 -0300

    Upload lesson

---
+++ Lessons/React/02-States/src/MyComponent.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+
+export const MyComponent = () => {
+	const [state, setState] = useState(0);
+
+	const increase = () => {
+		setState(state + 1);
+	};
+
+	const decrease = () => {
+		setState(state - 1);
+	};
+
+	return (
+		<div>
+			<h1>
+				Contador <span>{state}</span>
+			</h1>
+			<button onClick={increase}>+</button>
+			<button onClick={decrease}>-</button>
+		</div>
+	);
+};
