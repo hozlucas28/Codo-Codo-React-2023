2ddffaef2b593d0f6fb3b405d4d1ff90b6b66343
hozlucas28 <hozlucas28@gmail.com>
2023-03-27 20:06:47 -0300

    Upload lesson

---
+++ Lessons/React/03-Styles/src/components/MyComponent.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './MyComponent.module.css'
+
+const MyComponent = () => {
+	return (
+		<div>
+			<div className={styles.container}>¡Hola Mundo!</div>
+		</div>
+	)
+}
+
+export default MyComponent
